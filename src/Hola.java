acaf2ba997662c842a378ea4bf7f4fa504491ec0
Alejandro Ortega Moreno <aortmor509@g.educaand.es>
2026-01-14 13:27:44 +0100

    Version 2.0

--- src/Hola.java
+++ src/Hola.java
@@ -2,5 +2,11 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
+
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+
+
+
     }
 }
